8b1a50a76c22a783317c293b96416673f679d74d
sackidude <nils.nilsen9@outlook.com>
2024-08-07 12:59:45 +0200

    triangle

---
+++ src/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const mach = @import("mach");
+
+pub const modules = .{
+    mach.Core,
+    @import("App.zig"),
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    var app = try mach.App.init(allocator, .app);
+    defer app.deinit(allocator);
+    try app.run(.{ .allocator = allocator });
+}
